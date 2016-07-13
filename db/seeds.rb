70e07aa3915d7950acd00722a16570b17ba7493d
Thomas Sertorio <thomas.sertorio@gmail.com>
2016-07-13 09:43:47 +0200

    Add Geocoding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Pokemon.destroy_all
+Pokemon.create(name: "Aspifion", address: "Place du massacre, Nantes")
+Pokemon.create(name: "Aspifouette", address: "12 Rue Paul Bellamy, Nantes")
+Pokemon.create(name: "Pikachu", address: "27 Rue Paul Bellamy, Nantes")
+Pokemon.create(name: "Raichu", address: "29 Rue Paul Bellamy, Nantes")
+Pokemon.create(name: "Carapuce", address: "45 Rue Paul Bellamy, Nantes")
+Pokemon.create(name: "Tortank", address: "56 Rue Paul Bellamy, Nantes")
+Pokemon.create(name: "Dracaufeu", address: "56 Rue de la Bastille, Nantes")
+Pokemon.create(name: "Salameche", address: "48 Rue de la Bastille, Nantes")
+Pokemon.create(name: "Bulbizard", address: "4 rue Chaptal, Paris")
